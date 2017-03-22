68d665134210c9f0a7f6f76a776f6f4addf83d5c
Dean Merchant <dean.merchant@softwire.com>
2017-03-22 19:47:55 +0000

    Can now supply the directory to serve and port to run on as command line arguments.

--- Readme.md
+++ Readme.md
@@ -3,4 +3,12 @@
 - Run `npm install`
 - Put your files in /public
 - Run `npm start`
-- Go to (http://localhost:3000/)
+- Go to (http://localhost:8000/)
+
+By default the 'public' directory will be served on port 8000, this is customizable by supplying command line arguments e.g.
+```
+./index.js --path /path/to/directory --port 3210
+```
+
+All the files, folders and subfolders will be available and if there is an index.html files present in the root folder, that will be served up at `'/'`
+ 
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+#!/usr/bin/env node
 const express = require('express')
+const { readdirSync } = require('fs')
+const { resolve, join } = require('path')
+const { path, port } = require('yargs').argv
 
 const app = express();
+const appPort = port || process.env.PORT || 8000
+const directoryPath = resolve(path || 'public')
+const indexFile = readdirSync(directoryPath).filter(file => file.name === 'index.html').length > 0
+  ? join(directoryPath, 'index.html')
+  : join(__dirname, '/public/index.html')
 
-app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+app.use(express.static(directoryPath))
+app.get('*', (req, res) => res.sendFile(indexFile))
 
-const port = process.env.PORT || 3000
-app.listen(port, (err) => {
+app.listen(appPort, (err) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
 
-  console.log(`Started on port: ${port}`)
+  console.log(`Started on port: ${appPort}`)
+  console.log(`Serving: ${directoryPath}`)
 })
